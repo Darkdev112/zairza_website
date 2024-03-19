87028e6dbb295ad16ca552fc7e891e6c3e17fe59
paras-14 <parasbhatia100@gmail.com>
2024-03-20 00:34:49 +0530

    Updated Team And Alumni

---
+++ src/Pages/Teams/teamJSON.jsx
@@ -0,0 +1,141 @@
+const teamData=[
+    {
+        detailNo:1,
+        img:require("../../Assets/images/teams/jyoti.png"),
+        title: "Jyoti Nanda",
+        desc: "Convenor",
+    },
+    {
+        detailNo:2,
+        img:require("../../Assets/images/teams/2.webp"),
+        title: "Ashutosh Panda",
+        desc: "Coordinator",  
+    },
+    {
+        detailNo:3,
+        img:require("../../Assets/images/teams/3.webp"),
+        title: "Subhasmita Pradhan",
+        desc: "Design Mentor",
+    },
+    {
+        detailNo:4,
+        img:require("../../Assets/images/teams/adarsh.png"),
+        title: "Adarsh Lal",
+        desc: "ME (2024)",
+    },
+    {
+        detailNo:5,
+        img:require("../../Assets/images/teams/akanksha.webp"),
+        title: "Akanksha Panda",
+        desc: "EIE (2024)",
+    },
+    {
+        detailNo:6,
+        img:require("../../Assets/images/teams/Frame 7533.png"),
+        title: "Ashish Mohapatra",
+        desc: "IT (2024)",
+    },
+
+    {
+        detailNo:7,
+        img:require("../../Assets/images/teams/mohanty.png"),
+        title: "Ashutosh Mohanty",
+        desc: "IT (2024)",
+    },
+    {
+        detailNo:8,
+        img:require("../../Assets/images/teams/ayush.png"),
+        title: "Ayush Acharya",
+        desc: "IT (2024)",
+    },
+    {
+        detailNo:9,
+        img:require("../../Assets/images/teams/binaya.webp"),
+        title: "Binay Parida",
+        desc: "EIE (2024)",
+    },
+    {
+        detailNo:10,
+        img:require("../../Assets/images/teams/biswa.png"),
+        title: "Biswakiran Das",
+        desc: "IT (2024)",
+    },
+    {
+        detailNo:11,
+        img:require("../../Assets/images/teams/gourav.webp"),
+        title: "Gourav Sahoo",
+        desc: "ME (2024)",
+    },
+    {
+        detailNo:12,
+        img:require("../../Assets/images/teams/random.webp"),
+        title: "Jyoti Prakash Sahu",
+        desc: "IT (2024)",
+    },
+    {
+        detailNo:13,
+        img:require("../../Assets/images/teams/mukti.webp"),
+        title: "Mukti Priyadarshi Ray",
+        desc: "ME (2024)",
+    },
+    {
+        detailNo:14,
+        img:require("../../Assets/images/teams/pankaj.webp"),
+        title: "Pankaj Agarwal",
+        desc: "IT (2024)",
+    },
+    {
+        detailNo:15,
+        img:require("../../Assets/images/teams/pratik.png"),
+        title: "Pratik Agarwal",
+        desc: "CSE (2024)",
+    },
+
+
+    {
+        detailNo:16,
+        img:require("../../Assets/images/teams/pritam.webp"),
+        title: "Pritam Kumar Dash",
+        desc: "CSE (2024)",
+    },
+
+    {
+        detailNo:17,
+        img:require("../../Assets/images/teams/sai.webp"),
+        title: "Sai Soumyak Nanda",
+        desc: "IT (2024)",
+    },
+    {
+        detailNo:18,
+        img:require("../../Assets/images/teams/sameer.webp"),
+        title: "Sameer Dash",
+        desc: "IT (2024)",
+    },
+    {
+        detailNo:17,
+        img:require("../../Assets/images/teams/sup.png"),
+        title: "SSiddharth Mohanty",
+        desc: "CSE (2024)",
+    },
+    {
+        detailNo:20,
+        img:require("../../Assets/images/teams/random.webp"),
+        title: "Suprasanna Das ",
+        desc: "EE (2024)",
+    },
+    {
+        detailNo:21,
+        img:require("../../Assets/images/teams/tamana.webp"),
+        title: "Tamanna Swain",
+        desc: "EIE (2024)",
+    },
+    {
+        detailNo:21,
+        img:require("../../Assets/images/teams/vivek.webp"),
+        title: "Vivek Parida",
+        desc: "IT (2024)",
+    }
+]
+
+
+export default teamData;
